045686760eb754e934a652cfca0c9fc802d44e8d
Dmitry <crot26staww@gmail.ru>
2026-02-10 10:25:17 +0300

    feat: init eslint

---
+++ app/layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="layout-default">
+    <p>Heder</p>
+    <main>
+      <slot />
+    </main>
+    <p>footer</p>
+  </div>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  modules: ['@nuxt/image', 'nuxt-swiper', '@nuxt/eslint'],
+
+  css: [
+    '~/assets/styles/base/main.scss',
+  ],
+
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData:
+            '@use "~/assets/styles/base/global.scss" as *;',
+        },
+      },
+    },
+  },
+  eslint: {
+    config: {
+      stylistic: true // <---
+    }
+  }
 })
